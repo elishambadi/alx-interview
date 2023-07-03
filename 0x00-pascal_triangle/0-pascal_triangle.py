574becc4f22487aa3684b58954ddcb834b03c1ea
elishambadi <embadi43@gmail.com>
2023-07-03 05:37:06 +0300

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+def pascal_triangle(n):
+    """
+    Returns a pascal Triangle sequence
+    Args: n - height of triangle
+    Returns: array holding the numbers
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
