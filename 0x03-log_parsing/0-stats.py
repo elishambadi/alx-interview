0b213e0f469a65ed0cb72ee5182c5acae98f965d
= <=>
2023-10-22 19:00:33 +0300

    pycodestyle fixes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,14 +8,15 @@ status_code_counts = {}
 total_file_size = 0
 line_count = 0
 
+
 def print_statistics(signal, frame):
     """Prints Log statistics when Ctrl+c signal is passed"""
     print("Total file size:", total_file_size)
 
     for status_code in sorted(status_code_counts.keys()):
         print(f"{status_code}: {status_code_counts[status_code]}")
-    
     sys.exit(0)
+
 
 signal.signal(signal.SIGINT, print_statistics)
 
@@ -28,16 +29,15 @@ try:
                 file_size = int(parts[6])
             except ValueError:
                 continue
-
             total_file_size += file_size
-            
+
             if status_code in status_code_counts:
                 status_code_counts[status_code] += 1
             else:
                 status_code_counts[status_code] = 1
-            
+
             line_count += 1
-            
+
             if line_count % 10 == 0:
                 print("Total file size:", total_file_size)
                 for status_code in sorted(status_code_counts.keys()):
