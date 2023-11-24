4df0a6cf629443d27e180e8a38a2194a4a5fdc8f
elishambadi <embadi43@gmail.com>
2023-11-24 17:26:25 +0300

    min coins

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def makeChange(coins, total):
+    # If total is 0 or less, return 0
+    if total <= 0:
+        return 0
+
+    min_coins = [float('inf')] * (total + 1)
+    min_coins[0] = 0  # Base case: 0 coins needed to make 0 total
+
+    # Compute the minimum coins needed for all values from 1 to total
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if coin <= amount:
+                res = min(min_coins[amount], min_coins[amount - coin] + 1)
+                min_coins[amount] = res
+
+    # If the total cannot be met by any number of coins, return -1
+    return min_coins[total] if min_coins[total] != float('inf') else -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,3 @@
+# Making Change - Interview Question
+
+Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total
